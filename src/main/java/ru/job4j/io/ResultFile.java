2485fa2616dcfe8f081bd659ed0ea29a6a47d049
Dmitriy Kupreev <d3_october@yahoo.com>
2023-07-21 17:12:30 +0300

    2.0. FileOutputStream [#252488 #431205]_2

--- src/main/java/ru/job4j/io/ResultFile.java
+++ src/main/java/ru/job4j/io/ResultFile.java
@@ -1,14 +1,17 @@
 package ru.job4j.io;
 
 import java.io.FileOutputStream;
-import java.util.Arrays;
 
 public class ResultFile {
-    public static int[][] multiple(int size) {
-        int[][] array = new int[size][size];
+    public static String[][] multiple(int size) {
+        String[][] array = new String[size][size];
         for (int i = 0; i < size; i++) {
             for (int j = 0; j < size; j++) {
-                array[i][j] = (i + 1) * (j + 1);
+                if ((i + 1) * (j + 1) < 10) {
+                    array[i][j] = "" + (j + 1) + "x" + (i + 1) + "=" + (i + 1) * (j + 1) + " ";
+                } else {
+                    array[i][j] = "" + (j + 1) + "x" + (i + 1) + "=" + (i + 1) * (j + 1);
+                }
             }
         }
         return array;
@@ -16,9 +19,22 @@ public class ResultFile {
 
     public static void main(String[] args) {
         try (FileOutputStream out = new FileOutputStream("data/dataresult.txt")) {
-            out.write("Hello, world!".getBytes());
+            out.write("................................................................................".getBytes());
             out.write(System.lineSeparator().getBytes());
-            out.write(Arrays.deepToString(multiple(9)).getBytes());
+            out.write("Multiplication table!".getBytes());
+
+            out.write(System.lineSeparator().getBytes());
+            out.write("................................................................................".getBytes());
+            String[][] array = ResultFile.multiple(9);
+            out.write(System.lineSeparator().getBytes());
+            for (String[] r : array) {
+                for (String c : r) {
+                    out.write((c + " | ").getBytes());
+                }
+                out.write(System.lineSeparator().getBytes());
+                out.write(".......|........|........|........|........|........|........|........|.........".getBytes());
+                out.write(System.lineSeparator().getBytes());
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
