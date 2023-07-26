53ee05b9c5160e146cdc0398e3bfb69a5897664a
Dmitriy Kupreev <d3_october@yahoo.com>
2023-07-26 16:21:14 +0300

    7. Анализ доступности сервера [#859 #432369]_4

--- src/main/java/ru/job4j/io/Analysis.java
+++ src/main/java/ru/job4j/io/Analysis.java
@@ -3,30 +3,22 @@ package ru.job4j.io;
 import java.io.*;
 
 public class Analysis {
-    private final StringBuilder out = new StringBuilder();
-    private int range = 0;
+    private boolean range = true;
 
     public void unavailable(String source, String target) {
         try (BufferedReader reader = new BufferedReader(new FileReader(source));
              PrintWriter writer = new PrintWriter(new BufferedWriter(new FileWriter(target)))) {
             reader.lines().forEach(line -> {
-                if ((line.startsWith("400")
-                        || line.startsWith("500"))
-                && range == 0) {
-                    out.append(line.substring(4));
-                    out.append(";");
-                    range = 1;
+                boolean work = Integer.parseInt(line.substring(0, 1)) > 3;
+                if (work && range) {
+                    writer.print(line.substring(4) + ";");
+                    range = false;
                 }
-                if ((line.startsWith("200")
-                        || line.startsWith("300"))
-                        && range == 1) {
-                    out.append(line.substring(4));
-                    out.append(";");
-                    range = 0;
-                    out.append("\n");
+                if (!work && !range) {
+                    writer.println(line.substring(4) + ";");
+                    range = true;
                 }
             });
-            writer.println(out);
         } catch (
                 IOException e) {
             e.printStackTrace();
