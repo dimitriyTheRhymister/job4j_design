d2223ea8b2c09d4cd7fe3d4d60f36d0b0b9d9ae1
Dmitriy Kupreev <d3_october@yahoo.com>
2023-08-14 16:35:39 +0300

    7. Балансир [#505083]

---
+++ src/main/java/ru/job4j/iterator/Balancer.java
@@ -0,0 +1,28 @@
+package ru.job4j.iterator;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class Balancer {
+    public static void split(List<ArrayList<Integer>> nodes, Iterator<Integer> source) {
+        while (source.hasNext()) {
+            for (ArrayList<Integer> integerArrayList : nodes) {
+                integerArrayList.add(source.next());
+                if (!source.hasNext()) {
+                    break;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        List<ArrayList<Integer>> nodes = List.of(
+                new ArrayList<>(),
+                new ArrayList<>()
+        );
+        Iterator<Integer> source = List.of(1, 2, 3).iterator();
+        Balancer.split(nodes, source);
+        System.out.println(nodes);
+    }
+}
