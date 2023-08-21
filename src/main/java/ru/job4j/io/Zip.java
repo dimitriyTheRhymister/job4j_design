1cc46c5c06dabdb9fd786eafb901bc2ef61aa985
Dmitriy Kupreev <d3_october@yahoo.com>
2023-08-21 15:08:58 +0300

    14. Архивировать проект [#861 #438074]_5

--- src/main/java/ru/job4j/io/Zip.java
+++ src/main/java/ru/job4j/io/Zip.java
@@ -7,37 +7,31 @@ import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
 public class Zip {
-    private void validateArgs(String[] args) {
-        if (args.length != 3) {
-            throw new IllegalArgumentException("The required number of Arguments was not passed to the program");
-        }
-        File source = new File(args[0].substring(3));
+    private void validateArgs(ArgsName args) {
+        File source = new File(args.get("d"));
         if (!source.exists()) {
             throw new IllegalArgumentException(String.format("Not exist %s", source.getAbsoluteFile()));
         }
-        boolean exclude = args[1].substring(4).startsWith(".");
+        boolean exclude = args.get("e").startsWith("*.");
         if (!exclude) {
             throw new IllegalArgumentException("Wrong extension format for exclude argument");
         }
-        String target = args[2].split("\\.")[1];
-        if (!target.equals("zip")) {
+        String target = args.get("o");
+        if (!target.equals("project.zip")) {
             throw new IllegalArgumentException("Wrong target extension format");
         }
-        /*System.out.println("Валидация аргументов прошла успешно");*/
     }
 
     private void packFiles(String sourceDir, String extension, String zipFile) {
         try (ZipOutputStream zos = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(zipFile)))) {
             File dirSource = new File(sourceDir);
             addDirectory(zos, extension, dirSource);
-            /*System.out.println("Zip файл создан!");*/
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
     private void addDirectory(ZipOutputStream zos, String extension, File dirSource) throws IOException {
-        /*System.out.println("Добавление директории <" + dirSource.getName() + ">");*/
         Path start = dirSource.toPath();
         List<Path> sources = Search.search(start, p -> !p.toString().endsWith(extension));
         for (Path p : sources) {
@@ -51,7 +45,6 @@ public class Zip {
     }
 
     private void addFile(ZipOutputStream zos, File fileSource) {
-        /*System.out.println("Добавление файла <" + fileSource.getName() + ">");*/
         try (BufferedInputStream bis = new BufferedInputStream(new FileInputStream(fileSource))) {
             zos.putNextEntry(new ZipEntry(fileSource.getPath()));
             zos.write(bis.readAllBytes());
@@ -61,8 +54,12 @@ public class Zip {
     }
 
     public static void main(String[] args) throws IOException {
+        if (args.length != 3) {
+            throw new IllegalArgumentException("The required number of Arguments was not passed to the program");
+        }
+        ArgsName argsName = ArgsName.of(args);
         Zip zipDir = new Zip();
-        zipDir.validateArgs(args);
+        zipDir.validateArgs(argsName);
         String sourceDir = args[0].substring(3);
         String extension = args[1].substring(4);
         String zipFile = args[2].substring(3);
