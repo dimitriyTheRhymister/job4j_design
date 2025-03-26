f9e5f41c8c5a0598f62100db495c0cce3abb931d
Dmitriy Kupreev <d3_october@yahoo.com>
2025-03-26 16:16:16 +0300

    Триггеры [#504803]_3

--- db/trigger.sql
+++ db/trigger.sql
@@ -26,9 +26,7 @@ or replace function tax_row_before()
     returns trigger as
 $$
     BEGIN
-        update products
-        set price = price + price/2
-        where id = new.id;
+        NEW.price = NEW.price + NEW.price/2;
         return NEW;
     END;
 $$
